4b85db042b63ce5af32a92c8e3d11f004930f304
soumith <soumith@fb.com>
2015-08-05 04:38:38 -0700

    soft-fallback on not loading sundown

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
-local sd = require 'sundown'
-
 dok = {}
 
 require 'dok.inline'
 
-dok.markdown2html = sd.render
+local ok,sd = pcall(require, 'sundown')
+if ok then
+    dok.markdown2html = sd.render
+else
+    dok.markdown2html = function() return '<p> Error: Sundown could not be loaded </p>' end
+end
